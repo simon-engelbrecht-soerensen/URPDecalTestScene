64eb6303cd599f29bb4259655e08995b5475558e
Simon Engelbrecht Sørensen <s.engelbrecht@gmail.com>
2021-06-14 17:53:29 +0200

    1st

---
+++ Assets/Scripts/LightBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightBar : MonoBehaviour
+{
+    [SerializeField]
+    private DecalProjector decalProjector;
+
+    private MeshRenderer mRenderer;
+    void Start()
+    {
+        // Material newMat = new Material(mRenderer.sharedMaterial);
+        // newMat.SetColor("_Color", );
+    }
+
+}
